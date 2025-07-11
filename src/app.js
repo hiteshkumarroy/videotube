05c8393584ee830f6aec4878947a15e5989bc561
Hitesh Kumar Roy <hiteshroy011@gmail.com>
2025-07-11 20:06:53 +0530

    healthcheck routes and testing

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express"
 import cors from "cors"
 
+
 const app=express();
 
 app.use(cors({
   origin:process.env.CORS_ORIGIN,
   credentials:true
 }))
+//import routes
+import { healthcheckRouter } from "./routes/healthcheck.routes.js"; 
 
+app.use("/api/v1/healthcheck",healthcheckRouter);
 
 export {app}
