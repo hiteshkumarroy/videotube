5c988b7698569823becf118dbe61cc8c35309a00
Hitesh Kumar Roy <hiteshroy011@gmail.com>
2025-08-12 16:32:37 +0530

    dahsboard controller and routes add

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,46 @@
+import { Video } from "../models/video.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import { Like } from "../models/like.model.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const getChannelStats = asyncHandler(async (req, res) => {
+    const channelId = req.user._id
+
+    // All videos for this channel
+    const videos = await Video.find({ owner: channelId }, "views duration _id")
+
+    const totalVideos = videos.length
+    const totalViews = videos.reduce((sum, v) => sum + v.views, 0)
+    const totalDuration = videos.reduce((sum, v) => sum + v.duration, 0)
+
+    const totalSubscribers = await Subscription.countDocuments({ channel: channelId })
+    const totalLikes = await Like.countDocuments({ video: { $in: videos.map(v => v._id) } })
+
+    res.status(200).json(
+        new ApiResponse(200, {
+            totalVideos,
+            totalViews,
+            totalDuration,
+            totalSubscribers,
+            totalLikes
+        })
+    )
+})
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+    const channelId = req.user._id
+    const { page = 1, limit = 10 } = req.query
+
+    const videos = await Video.find({ owner: channelId })
+        .sort({ createdAt: -1 })
+        .skip((page - 1) * limit)
+        .limit(Number(limit))
+
+    res.status(200).json(new ApiResponse(200, videos))
+})
+
+export {
+    getChannelStats,
+    getChannelVideos
+}
